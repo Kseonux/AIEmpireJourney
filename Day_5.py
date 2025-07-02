4fe72200cfa8a37d8e2814d8978993fde077cd2d
Kseonux <7a.giuseppeansellonardotelaumb@gmail.com>
2025-07-02 22:57:43 +0700

    Day 5 - A little fire

---
+++ Day_5.py
@@ -0,0 +1,3 @@
+print("Day 5 – Ive seen dark in my deepest heart. But also a little fire burning inside me The Future ME)' > Day_5.py
+
+echo 'print(Day 5 – Ive seen dark in my deepest heart. But also a little fire burning inside me The Future ME")
