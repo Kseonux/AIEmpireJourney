36724c23b8c7fbf321de6e64ec5f93706535a2fe
Kseonux <7a.giuseppeansellonardotelaumb@gmail.com>
2025-07-04 22:17:57 +0700

    Update

--- Day_5.py
+++ Day_5.py
@@ -1,3 +1,1 @@
-print("Day 5 – Ive seen dark in my deepest heart. But also a little fire burning inside me The Future ME)' > Day_5.py
-
-echo 'print(Day 5 – Ive seen dark in my deepest heart. But also a little fire burning inside me The Future ME")
+print(Day 5 – Ive seen dark in my deepest heart. But also a little fire burning inside me The Future ME")
